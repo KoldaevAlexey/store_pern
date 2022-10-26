ef8b75210e6df3e2d8eae86cfbcb80e19b41b31c
KoldaevAlexey <koldaev.alexey@gmail.com>
2022-10-27 02:07:02 +0300

    прописал роуты, потестил через инсомнию

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db");
+const cors = require("cors");
+
+const models = require("./models/models");
+const router = require("./routes/index");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", router);
 
-app.listen(PORT, () => {
-    console.log(`Server OK, PORT:${PORT} `);
-});
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => {
+            console.log(`Server OK, PORT:${PORT} `);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
