d58c5fb6665f84b5a16d538c6e8fc34b8a08520d
KoldaevAlexey <koldaev.alexey@gmail.com>
2022-11-01 23:45:39 +0300

    добавил типы, девайсы и бренды в БД, проверил работу get и put запросов

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,7 +1,63 @@
-class DeviceController {
-    async create(req, res) {}
+const uuid = require("uuid");
+const path = require("path");
 
-    async getAll(req, res) {}
+const { Device } = require("../models/models");
+const ApiError = require("../error/ApiError");
+
+class DeviceController {
+    async create(req, res, next) {
+        try {
+            const { name, price, brandId, typeId, info } = req.body;
+            const { img } = req.files;
+            let fileName = uuid.v4() + ".jpg";
+            img.mv(path.resolve(__dirname, "..", "static", fileName));
+
+            const device = await Device.create({
+                name,
+                price,
+                brandId,
+                typeId,
+                info,
+                img: fileName,
+            });
+            return res.json(device);
+        } catch (e) {
+            next(ApiError.badRequest(e.message));
+        }
+    }
+
+    async getAll(req, res) {
+        let { brandId, typeId, limit, page } = req.body;
+        page = page || 1;
+        limit = limit || 9;
+        let offset = page * limit - limit;
+        let devices;
+        if (!brandId && !typeId) {
+            devices = await Device.findAndCountAll({ limit, offset });
+        }
+        if (brandId && !typeId) {
+            devices = await Device.findAndCountAll({
+                where: { brandId },
+                limit,
+                offset,
+            });
+        }
+        if (!brandId && typeId) {
+            devices = await Device.findAndCountAll({
+                where: { typeId },
+                limit,
+                offset,
+            });
+        }
+        if (brandId && typeId) {
+            devices = await Device.findAndCountAll({
+                where: { typeId, brandId },
+                limit,
+                offset,
+            });
+        }
+        res.json(devices);
+    }
 
     async getOne(req, res) {}
 }
