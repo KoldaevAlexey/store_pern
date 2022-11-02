75096270f02d65fd0e2e574844815d8babf2771b
KoldaevAlexey <koldaev.alexey@gmail.com>
2022-11-02 23:15:46 +0300

    закончил с серверной частью, начал писать клиентскую часть

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import { authRoutes } from "../routes";
+
+const AppRouter = () => {
+    const isAuth = false;
+
+    return (
+        <Switch>
+            {authRoutes.map(({ path, Component }) => (
+                <Route key={path} path={path} component={Component} exact />
+            ))}
+        </Switch>
+    );
+};
+
+export default AppRouter;
--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,7 +1,7 @@
 const uuid = require("uuid");
 const path = require("path");
 
-const { Device } = require("../models/models");
+const { Device, DeviceInfo } = require("../models/models");
 const ApiError = require("../error/ApiError");
 
 class DeviceController {
@@ -11,15 +11,25 @@ class DeviceController {
             const { img } = req.files;
             let fileName = uuid.v4() + ".jpg";
             img.mv(path.resolve(__dirname, "..", "static", fileName));
-
             const device = await Device.create({
                 name,
                 price,
                 brandId,
                 typeId,
-                info,
                 img: fileName,
             });
+
+            if (info) {
+                info = JSON.parse(info);
+                info.forEach((i) =>
+                    DeviceInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id,
+                    })
+                );
+            }
+
             return res.json(device);
         } catch (e) {
             next(ApiError.badRequest(e.message));
@@ -59,7 +69,14 @@ class DeviceController {
         res.json(devices);
     }
 
-    async getOne(req, res) {}
+    async getOne(req, res) {
+        const { id } = req.params;
+        const device = await Device.findOne({
+            where: { id },
+            include: [{ model: DeviceInfo, as: "info" }],
+        });
+        return res.json(device);
+    }
 }
 
 module.exports = new DeviceController();
